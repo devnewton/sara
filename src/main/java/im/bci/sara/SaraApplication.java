fa3d220319a0620334f794e83f6bf475b86e3336
devnewton <devnewton@bci.im>
2017-06-09 11:13:56 +0200

    code clean

--- src/main/java/im/bci/sara/SaraApplication.java
+++ src/main/java/im/bci/sara/SaraApplication.java
@@ -1,18 +1,10 @@
 package im.bci.sara;
 
-import java.util.concurrent.Executor;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.SimpleThreadScope;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableAutoConfiguration
